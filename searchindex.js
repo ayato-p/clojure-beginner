54763f44d3bd9168c71a174d13e5b74c15b4c52d
ayato_p <ayato.skycrawlers@gmail.com>
2015-05-10 20:04:16 -0700

    first build

---
+++ searchindex.js
@@ -0,0 +1,1 @@
+Search.setIndex({envversion:42,terms:{content:0,index:0,modul:0,search:0,page:0},objtypes:{},objnames:{},filenames:["index"],titles:["Welcome to clojure-beginner&#8217;s documentation!"],objects:{},titleterms:{welcom:0,indic:0,clojur:0,tabl:0,document:0,beginn:0}})
